079a5deedd44e9ce4de7c31f077c9c7c354d6456
Shakshi Das <96143224+Shakshi-das@users.noreply.github.com>
2023-08-02 21:36:06 +0530

    Create Day +22.cpp
    Cpp program on BST

---
+++ Day +22.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstddef>
+using namespace std;
+
+class Node{
+   public:
+
+     int data;
+     Node *left; Node *right;
+
+     Node(int d) {
+        data = d;
+        left = NULL; right = NULL;
+    }
+};
+
+class Solution{
+   public:
+
+     Node* insert (Node* root,int data) 
+    {
+           if(root==NULL){
+             return new Node (data);
+           }
+          else {
+             Node* cur; 
+             if(data <=root->data) {
+                 cur insert (root->left, data);
+                 root->left= cur;
+             }
+           return root;
+          }
+     }
+
+int getHeight(Node* root){
+          //Write your code here
+          return root == NULL ? -1 : 1 + max(getHeight(root->left),                getHeight(root->right));
+}
